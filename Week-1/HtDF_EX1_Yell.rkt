f9eb4c7d2c06fc33a6e86295386879b29f372bab
v2chrisb <chrisbeattie@live.com>
2013-09-10 14:57:59 -0400

    HtDF examples

--- Week-1/HtDF_EX1_Yell.rkt
+++ Week-1/HtDF_EX1_Yell.rkt
@@ -1,8 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_EX1_Yell) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-; HtDF recipe yell!
-
 ;DESIGN a function called yell that consumes strings like "hello" 
 ;and adds "!" to produce strings like "hello!".
 ;
@@ -10,15 +8,16 @@
 ;
 ;Leave behind commented out versions of the stub and template.
 
-;; String -> String         ; signature
-;; add "!" to the end of s  ; purpose
+;; String -> String                             ; this is the signature
+;; produce one string by adding two strings     ; this is the purpose
 (check-expect (yell "hello") "hello!")
 (check-expect (yell "bye") "bye!")
 
-;(define (yell s) "")        ; stub
-
-;(define (yell s)            ; template
+;(define (yell s) "")                            ; this is the stub
+;(define (yell s)                                ; this is a template
 ;  (... s))
 
 (define (yell s)
   (string-append s "!"))
+
+(yell "hooya")
--- Week-1/HtDF_EX2_Area-of-Square.rkt
+++ Week-1/HtDF_EX2_Area-of-Square.rkt
@@ -1,8 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDF_EX2_Area-of-Square) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
-; HtDF recipe area
-
 ;DESIGN a function called area that consumes the length of one side 
 ;of a square and produces the area of the square.
 ;
@@ -10,19 +8,15 @@
 ;
 ;Leave behind commented out versions of the stub and template.
 
-;; Number -> Number                              ; signature
-;; given the length of one side of square, produce the areal
-;; of a square                                   ; purpose
+;; Number -> Number                                ; this is the signature
+;; produce the area of a square by 
+;; multiplying one side's length by itself         ; this is the purpose
+(check-expect (area 3) (* 3 3))
+(check-expect (area 2.3) (* 2.3 2.3))
 
-
-;; (define area n) 0)                            ; stub
-
-;(define (area n)
-;  (... n))                                      ; template
-(check-expect (area 3) 9)
-(check-expect (area 3.2) (* 3.2 3.2))
+;(define (area n) 0)                                ; this is the stub
 
 (define (area n)
   (* n n))
 
-
+(area 2.3)

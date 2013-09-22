78e8734b5c3c56aca5ee88736ce06ad487d0c49d
v2chrisb <chrisbeattie@live.com>
2013-09-21 22:30:06 -0400

    First five lectures of HtDW

---
+++ Week-3/HtDW_Template.rkt
@@ -0,0 +1,43 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HtDW_Template) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+;; My world program  (make this more specific)
+
+;; =================
+;; Constants:
+
+
+;; =================
+;; Data definitions:
+
+;; WS is ... (give WS a better name)
+
+
+
+;; =================
+;; Functions:
+
+;; WS -> WS
+;; start the world with ...
+;; 
+(define (main ws)
+  (big-bang ws                   ; WS
+            (on-tick   tock)     ; WS -> WS
+            (to-draw   render)   ; WS -> Image
+            (stop-when ...)      ; WS -> Boolean
+            (on-mouse  ...)      ; WS Integer Integer MouseEvent -> WS
+            (on-key    ...)))    ; WS KeyEvent -> WS
+
+;; WS -> WS
+;; produce the next ...
+;; !!!
+(define (tock ws) ...)
+
+
+;; WS -> Image
+;; render ... 
+;; !!!
+(define (render ws) ...)

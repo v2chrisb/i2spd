d455cd68eb306ca5bc11cc6abdc0ded37700c313
v2chrisb <chrisbeattie@live.com>
2013-09-29 19:48:08 -0400

    Final cleanup, added Grunt folders, start grass grow exercise

---
+++ Week-3/Grunt-3/P3_Compound-and-HtDW_growing-grass.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname P3_Compound-and-HtDW_growing-grass) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+;; HtDW_Compound_growing-grass.rkt

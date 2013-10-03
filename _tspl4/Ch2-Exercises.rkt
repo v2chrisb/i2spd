091cf5fe4cfb6ac5d161b1a27d4f20f5c9094d9f
v2chrisb <chrisbeattie@live.com>
2013-10-02 22:10:19 -0400

    Adding The Scheme Programming Lang v4 [tspl4] exercises and re-org of Webpage* PDFs

---
+++ _tspl4/Ch2-Exercises.rkt
@@ -0,0 +1,86 @@
+#lang racket
+;; Section 2.1
+
+;(car '(a b c))
+
+;(cdr '(a b c))
+
+;(cons 'a '(b c))
+
+;(cons (car '(a b c))
+;      (cdr '(d e f)))
+
+;(define square
+;  (lambda (n)
+;    (* n n)))
+
+;(define add1
+;  (lambda (a)
+;    (+ a 1)))
+
+;(define reciprocal
+;  (lambda (n)
+;    (if (= n 0)
+;        "oops!"
+;        (/ 1 n))))
+
+;; Exercise 2.2.1 -- convert arith. exp. to Scheme exp.
+;; 1.2 * (2 - 1/3) + -8.7
+; (- (* 1.2 (- 2 (/ 1 3))) 8.7)
+;; Ans. -6.69999~
+;; =====
+;; (2/3 + 4/9) / (5/11 - 4/3)
+; (define A (+ (/ 2 3) (/ 4 9)))
+; (define B (- (/ 5 11) (/ 4 3)))
+; (/ (+ (/ 2 3) (/ 4 9)) (- (/ 5 11) (/ 4 3)))
+;; Ans. -110/87
+;; =====
+;; 1 + 1 / (2 + 1 / (1 + 1/2))
+; (/ (+ 1 1) (/ (+ 2 1) (+ 1 (/ 1 2))))
+;; Ans. 1
+;; =====
+;; 1 * -2 * 3 * -4 * 5 * -6 * 7
+; (* 1 -2 3 -4 5 -6 7)
+;; Ans. -5040
+
+;; Exercise 2.2.3 -- determine the values of the following expressions.
+; (cons 'car 'cdr)
+;; Ans. (car . cdr)
+;; =====
+; (list 'this '(is silly))
+;; Ans. (this (is silly))
+;; =====
+; (cons 'is '(this silly?))
+;; Ans. (is this silly?)
+;; =====
+; (quote (+ 2 3))
+;; Ans. (+ 2 3)
+;; =====
+; (cons '+ '(2 3))
+;; Ans. (+ 2 3)
+;; =====
+; (car '(+ 2 3))
+;; Ans. +
+;; =====
+; (cdr '(+ 2 3))
+;; Ans. (2 3)
+;; =====
+; cons
+;; Ans. #<procedure cons>
+;; =====
+; (quote (quote cons))
+;; Ans. 'cons
+;; =====
+; (car (quote (quote cons)))
+;; =====
+; (+ 2 3)
+;; Ans. 5
+;; =====
+; (+ '2 '3)
+;; Ans. 5
+;; =====
+; (+ (car '(2 3)) (car (cdr '(2 3))))
+;; Ans. 5
+;; =====
+; ((car (list + - * /)) 2 3)
+;; Ans. 5
